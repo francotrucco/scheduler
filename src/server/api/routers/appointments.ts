4819583c10c06283ee8df9dd3439fce40e749c51
Franco Trucco <franco.trucco1@gmail.com>
2023-08-23 15:09:56 -0300

    Move inputs params to separate vars to imp clarity

--- src/server/api/routers/appointments.ts
+++ src/server/api/routers/appointments.ts
@@ -6,43 +6,34 @@ import {
 import { z } from "zod";
 import type { Prisma } from "@prisma/client";
 
-export const appointmentsRouter = createTRPCRouter({
-  get: publicProcedure
-    .input(z.object({ appointmentId: z.string() }))
-    .query((opts) => {
-      const { ctx, input } = opts;
+const appointmentIdInputSchema = z.object({ appointmentId: z.string() });
+const appointmentInputSchema = z.object({
+  appointmentId: z.string(),
+  appointmentTime: z.date(),
+  patientId: z.string(),
+  doctorId: z.string(),
+});
 
-      return ctx.prisma.appointments.findUnique({
-        where: { id: input.appointmentId },
-      });
-    }),
+export const appointmentsRouter = createTRPCRouter({
+  get: publicProcedure.input(appointmentIdInputSchema).query((opts) => {
+    const { ctx, input } = opts;
+
+    return ctx.prisma.appointments.findUnique({
+      where: { id: input.appointmentId },
+    });
+  }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.appointments.findMany();
   }),
-  add: privateProcedure
-    .input(
-      z.object({
-        appointmentTime: z.string(),
-        doctorId: z.string(),
-        patientId: z.string(),
-      })
-    )
-    .mutation((opts) => {
-      const appointment: Prisma.AppointmentsCreateArgs = {
-        data: { ...opts.input },
-      };
+  add: privateProcedure.input(appointmentInputSchema).mutation((opts) => {
+    const appointment: Prisma.AppointmentsCreateArgs = {
+      data: { ...opts.input },
+    };
 
-      return opts.ctx.prisma.appointments.create(appointment);
-    }),
+    return opts.ctx.prisma.appointments.create(appointment);
+  }),
   update: privateProcedure
-    .input(
-      z.object({
-        appointmentId: z.string(),
-        appointmentTime: z.date(),
-        patientId: z.string(),
-        doctorId: z.string(),
-      })
-    )
+    .input(appointmentInputSchema)
     .mutation(({ ctx, input }) => {
       const updatedAppointment: Prisma.AppointmentsUpdateArgs = {
         where: {
@@ -56,11 +47,7 @@ export const appointmentsRouter = createTRPCRouter({
       return ctx.prisma.appointments.update(updatedAppointment);
     }),
   delete: privateProcedure
-    .input(
-      z.object({
-        appointmentId: z.string(),
-      })
-    )
+    .input(appointmentIdInputSchema)
     .mutation(({ input, ctx }) => {
       return ctx.prisma.appointments.delete({
         where: { id: input.appointmentId },
